10870e7ecf975d6391d0dce3fb927e46161df365
Ushurova Nigora <nigora2552@gmail.com>
2026-02-26 23:51:33 +0600

    [frontend] Создала setInterval для получения всех сообщений

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -1,29 +1,29 @@
 import {useAppDispatch, useAppSelector} from "../../app/hooks.ts";
 import {useEffect} from "react";
-import {getMessages, loadingSelector, messagesSelector} from "../../app/messagesSlice.ts";
-import {CircularProgress, Paper} from "@mui/material";
+import {getMessages, messagesSelector} from "../../app/messagesSlice.ts";
+import { Paper} from "@mui/material";
 
 
 const Card = () => {
     const dispatch = useAppDispatch();
     const messages = useAppSelector(messagesSelector);
-    const loading = useAppSelector(loadingSelector);
+
 
     useEffect(() => {
         dispatch(getMessages())
+        const interval = setInterval(() => {dispatch(getMessages())}, 5000)
+        return () => clearInterval(interval)
     }, [dispatch])
 
 
     return (
         <>
-            {loading && <CircularProgress/>}
-            {!loading && messages.length === 0 && <p>No message</p>}
-            {!loading && messages.length > 0 &&
+            {messages.length > 0 &&
                 messages.map(msg => (
                     <Paper key={msg.id} sx={{padding: '10px 20px', marginY: '15px'}}>
                         <h4>{msg.author}</h4>
                         <p>{msg.message}</p>
-                        {msg.image ? <img src={msg.image} alt=''/> : <p>No photo</p>  }
+                        {msg.image ? <img width='50%' height='100%' src={msg.image} alt=''/> : <p>No photo</p>}
                     </Paper>
                 ))}
         </>
