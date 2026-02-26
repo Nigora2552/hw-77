f2a7b9b476d74c521869c0174a6ca1dd6c0c4915
Ushurova Nigora <nigora2552@gmail.com>
2026-02-26 19:36:05 +0600

    [frontend] Создала форму и инпут с типом file

---
+++ frontend/src/components/Form/Form.tsx
@@ -0,0 +1,82 @@
+import {Button, Container, Grid, TextField} from "@mui/material";
+import React, {useState} from "react";
+import type {MessageMutation} from "../../../types";
+import FileInput from "../UI/FileInput/FileInput.tsx";
+
+interface Props {
+    onSubmit: (item: MessageMutation) => void;
+}
+
+const Form: React.FC<Props> = ({onSubmit}) => {
+    const [form, setForm] = useState<MessageMutation>({
+        author: '',
+        message: '',
+        image: null
+    });
+
+    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setForm(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    };
+
+    const fileInputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, files} = e.target;
+        if(files){
+            setForm(prevState => ({
+                ...prevState,
+                [name]: files[0]
+            }))
+        }
+    };
+
+    const SubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+
+        onSubmit(form)
+    };
+
+
+    return (
+        <Container>
+            <form onSubmit={SubmitForm}>
+                <Grid spacing={2}>
+                    <Grid margin='15px 0'>
+                        <TextField
+                            fullWidth
+                            id="author"
+                            label="Author"
+                            variant="outlined"
+                            name='author'
+                            value={form.author}
+                            onChange={onInputChange}
+                        />
+                    </Grid>
+                    <Grid margin='15px 0'>
+                        <TextField
+                            fullWidth
+                            id="message"
+                            label="Message"
+                            variant="outlined"
+                            name='message'
+                            value={form.message}
+                            onChange={onInputChange}
+                        /></Grid>
+                    <Grid margin='15px 0'>
+                        <FileInput
+                            label='image'
+                            name='image'
+                            onChange={fileInputChangeHandler}
+                        />
+                    </Grid>
+                </Grid>
+                <Button type='submit' variant="outlined"> Add</Button>
+            </form>
+        </Container>
+    );
+};
+
+export default Form;
